735daa0bae768dd0b313d3ef25db7d4397cb8454
Naesa Orlina <norlina@thoughtworks.com>
2017-01-27 00:19:38 +1000

    add dashboard

--- index.js
+++ index.js
@@ -4,8 +4,14 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var ParseDashboard = require('parse-dashboard');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var MASTER_KEY = process.env.MASTER_KEY || '12345678'; //Add your master key here. Keep it secret!
+var IS_DEVELOPMENT = process.env.NODE_ENV !== 'production';
+var DASHBOARD_AUTH = process.env.DASHBOARD_AUTH || null;
+var SERVER_URL = process.env.SERVER_URL || 'http://localhost:1337/parse';  // Don't forget to change to https if needed
+var APP_ID = process.env.APP_ID || 'myAppId';
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
@@ -14,9 +20,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: APP_ID,
+  masterKey: MASTER_KEY,
+  serverURL: SERVER_URL,
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
@@ -33,6 +39,34 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
+
+
+
+if (IS_DEVELOPMENT) {
+  let users;
+  if (DASHBOARD_AUTH) {
+    var [user, pass] = DASHBOARD_AUTH.split(':');
+    users = [{user, pass}];
+    console.log(users);
+  }
+  var dashboard = ParseDashboard({
+      allowInsecureHTTP: true,
+      apps: [{
+        serverURL: SERVER_URL,
+        appId: APP_ID,
+        masterKey: MASTER_KEY,
+        appName: 'Team-Hug-App-2017',
+        javascriptKey: "NOT USED",
+        restKey: "NOT USED",
+      }],
+      users,
+    }, IS_DEVELOPMENT);
+
+    app.use( '/dashboard', dashboard);
+}
+
+
+
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
